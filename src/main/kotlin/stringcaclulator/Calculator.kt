284ce23940bbeed858b9385ed847e86c7f5e558c
RoadtoS7 <kshvv975@gmail.com>
2020-10-20 23:13:26 +0900

    refactor(Calculator.kt): apply code review
    수식, 숫자, 연산자의 유효성을 체크하는 Checker 클래스 객체를 인스턴스 변수로
    가지고 이를 이용하여 검증하는 기능을 수행하도록 했다.
    checkThenCalculate()가 검증하는 기능과 계산하는 기능 두가지를 수행하여,
    계산하는 기능만 수행하도록 하고 함수의 이름을 calculateOperator()로
    바꾸었다.
    checkThenCalculate()의 인자가 3개였는데 이를 줄이기 위해서 결과값을 담는
    변수를 클래스내에 result라는 이름의 인스턴스 변수로 만들었다.

--- src/main/kotlin/stringcaclulator/Calculator.kt
+++ src/main/kotlin/stringcaclulator/Calculator.kt
@@ -4,30 +4,33 @@ import java.lang.IllegalArgumentException
 import kotlin.math.round
 
 class Calculator {
+    private val checker = Checker()
+    private var result = 0.0
+
     fun calculate(input: String): Double {
-        val inputList = input.split(" ")
-        return calculateWithSplitList(inputList)
+        val mathExpression = input.split(" ")
+        checker.checkMathExpressionSize(mathExpression)
+        return calculateProgressively(mathExpression)
     }
 
-    fun calculateWithSplitList(splitList: List<String>): Double {
-        checkNumber(splitList[0])
-        var result = splitList[0].toDouble()
+    fun calculateProgressively(splitList: List<String>): Double {
+        checker.checkNumber(splitList[0])
+        result = splitList[0].toDouble()
 
         for (i in 1 until splitList.size step 2) {
             val operator = splitList[i]
             val number = splitList[i + 1]
-            result = checkThenCalculate(
+            checker.checkNumber(number)
+            checker.checkOperator(operator)
+            result = calculateByOperator(
                 operator,
-                result,
                 number
             )
         }
         return result
     }
 
-    fun checkThenCalculate(operator: String, result: Double, number: String): Double {
-        checkNumber(number)
-        checkOperator(operator)
+    private fun calculateByOperator(operator: String, number: String): Double {
         return when (operator) {
             "+" -> add(result, number.toDouble())
             "-" -> subtract(result, number.toDouble())
@@ -35,22 +38,6 @@ class Calculator {
             "/" -> divide(result, number.toDouble())
             else -> throw IllegalArgumentException("연산자 위치에 잘못된 값이 들어있습니다. 잘못된 연산자: $operator")
         }
-    }
-
-    fun checkNumber(number: String): Boolean {
-        val result = NUMBER_REGEX.matches(number)
-        if (!result) {
-            throw IllegalArgumentException("숫자 위치에 잘못된 값이 들어있습니다. 잘못된 숫자: $number")
-        }
-        return result
-    }
-
-    fun checkOperator(operator: String): Boolean {
-        val result = OPERATOR_REGEX.matches(operator)
-        if (!result) {
-            throw IllegalArgumentException("연산자 위치에 잘못된 값이 들어있습니다. 잘못된 연산자: $operator")
-        }
-        return result
     }
 
     fun add(number1: Double, number2: Double) = number1 + number2
@@ -63,9 +50,4 @@ class Calculator {
     fun divide(number1: Double, number2: Double) = number1 / number2
 
     fun multiply(number1: Double, number2: Double) = number1 * number2
-
-    companion object {
-        private val NUMBER_REGEX = "[0-9]+|[0-9]+[.][0-9]+".toRegex()
-        private val OPERATOR_REGEX = "[+]|[-]|[*]|[/]".toRegex()
-    }
 }
