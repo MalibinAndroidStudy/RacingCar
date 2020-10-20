89adfd6550367164888195180228c822a9c7e85b
RoadtoS7 <kshvv975@gmail.com>
2020-10-20 23:07:29 +0900

    feat(Checker.kt): add checkMathExpressionSize function to check mathExpressionSize
    입력 요소의 개수가 최소 3개는 되어야 수식이 되기 때문에 이를 확인하는 함수인 checkMathExpressionSize()를 만들었다.

---
+++ src/main/kotlin/stringcaclulator/Checker.kt
@@ -0,0 +1,28 @@
+package stringcaclulator
+
+import java.lang.IllegalArgumentException
+
+class Checker {
+    fun checkMathExpressionSize(mathExpression: List<String>) {
+        require(mathExpression.size >= 3) { throw IllegalArgumentException("연산식의 구성요소는 최소 3개여야 합니다.") }
+    }
+
+    fun checkNumber(number: String) {
+        val isNumber = NUMBER_REGEX.matches(number)
+        if (!isNumber) {
+            throw IllegalArgumentException("숫자 위치에 잘못된 값이 들어있습니다. 잘못된 숫자: $number")
+        }
+    }
+
+    fun checkOperator(operator: String) {
+        val isResult = OPERATOR_REGEX.matches(operator)
+        if (!isResult) {
+            throw IllegalArgumentException("연산자 위치에 잘못된 값이 들어있습니다. 잘못된 연산자: $operator")
+        }
+    }
+
+    companion object {
+        private val NUMBER_REGEX = "[0-9]+|[0-9]+[.][0-9]+".toRegex()
+        private val OPERATOR_REGEX = "[+]|[-]|[*]|[/]".toRegex()
+    }
+}
