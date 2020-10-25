a0a6f40a8905aea79fb19da9a0eb48aa4fb0b451
RoadtoS7 <kshvv975@gmail.com>
2020-10-25 20:28:19 +0900

    refactor(Calculator.kt): add enum class for operator
    연산자를 상수처리하기 위해서 enum class를 만들었다.
    그리고 enum class내에 연산자에 해당하는 객체가 존재하는지 확인하기 위한 함수를
    만들었다.

--- src/main/kotlin/stringcaclulator/Calculator.kt
+++ src/main/kotlin/stringcaclulator/Calculator.kt
@@ -3,51 +3,76 @@ package stringcaclulator
 import java.lang.IllegalArgumentException
 import kotlin.math.round
 
+enum class Operator(
+    private val operator: String,
+) {
+    PLUS("+"),
+    MINUS("-"),
+    MULTIPLY("*"),
+    DIVIDE("/");
+
+    fun hasSymbol(otherSymbol: String) =
+        this.operator == otherSymbol
+
+    companion object {
+        fun findOperator(symbol: String): Operator {
+            return values().find { it.hasSymbol(symbol) }
+                ?: throw IllegalArgumentException("잘못된 연산자 입니다. 잘못된 연산자: $symbol")
+        }
+    }
+}
+
 class Calculator {
-    private val checker = Checker()
-    private var result = 0.0
+    private var acc = 0.0
 
     fun calculate(input: String): Double {
         val mathExpression = input.split(" ")
-        checker.checkMathExpressionSize(mathExpression)
+        Checker.checkMathExpressionSize(mathExpression)
         return calculateProgressively(mathExpression)
     }
 
-    fun calculateProgressively(splitList: List<String>): Double {
-        checker.checkNumber(splitList[0])
-        result = splitList[0].toDouble()
+    private fun calculateProgressively(splitList: List<String>): Double {
+        Checker.checkNumber(splitList[0])
+        acc = splitList[0].toDouble()
 
         for (i in 1 until splitList.size step 2) {
-            val operator = splitList[i]
+            val symbol = splitList[i]
             val number = splitList[i + 1]
-            checker.checkNumber(number)
-            checker.checkOperator(operator)
-            result = calculateByOperator(
-                operator,
-                number
+            checkValidity(number, symbol)
+            acc = accumulateCalculation(
+                symbol,
+                number.toDouble()
             )
         }
-        return result
+        return acc
     }
 
-    private fun calculateByOperator(operator: String, number: String): Double {
+    private fun checkValidity(number: String, operator: String) {
+        Checker.checkNumber(number)
+        Checker.checkOperator(operator)
+    }
+
+    private fun accumulateCalculation(symbol: String, number: Double): Double {
+        val operator = Operator.findOperator(symbol)
         return when (operator) {
-            "+" -> add(result, number.toDouble())
-            "-" -> subtract(result, number.toDouble())
-            "*" -> multiply(result, number.toDouble())
-            "/" -> divide(result, number.toDouble())
-            else -> throw IllegalArgumentException("연산자 위치에 잘못된 값이 들어있습니다. 잘못된 연산자: $operator")
+            Operator.PLUS -> add(acc, number)
+            Operator.MINUS -> subtract(acc, number)
+            Operator.MULTIPLY -> multiply(acc, number)
+            Operator.DIVIDE -> divide(acc, number)
         }
     }
 
-    fun add(number1: Double, number2: Double) = number1 + number2
+    private fun add(number1: Double, number2: Double): Double {
+        val result = number1 + number2
+        return round(result * 1000) / 1000.0
+    }
 
-    fun subtract(number1: Double, number2: Double): Double {
+    private fun subtract(number1: Double, number2: Double): Double {
         val result = number1 - number2
         return round(result * 1000) / 1000.0
     }
 
-    fun divide(number1: Double, number2: Double) = number1 / number2
+    private fun multiply(number1: Double, number2: Double) = number1 * number2
 
-    fun multiply(number1: Double, number2: Double) = number1 * number2
+    private fun divide(number1: Double, number2: Double) = number1 / number2
 }
