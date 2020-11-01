425f36ac72fed611b91029ed733b9c72e5ede1f6
chaeppy <chaeppy108@gmail.com>
2020-11-01 23:59:22 +0900

    feat(step1): 1차 리팩토링

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,48 +1,24 @@
 package calculator
 
-class Calculator {
-    companion object {
-        private val ArithmeticOperations = listOf<String>("+", "-", "*", "/")
+object Calculator {
+    private val expressionSplit = ExpressionSplit
 
-        fun add(a: Int, b: Int): Int = a + b
-        fun subtract(a: Int, b: Int): Int = a - b
-        fun multiply(a: Int, b: Int): Int = a * b
-        fun divide(a: Int, b: Int): Int {
-            require(b != 0) { "0으로 나눌 수 없습니다." }
-            return a / b
-        }
-
-        fun checkNullOrBlank(expression: String?) {
-            require(!expression.isNullOrBlank()) { "입력 값이 null 이거나 공백 문자열 입니다." }
-        }
-
-        fun checkArithmeticOperations(operator: String) {
-            require(ArithmeticOperations.contains(operator)) { "${operator}는 사칙연산 기호가 아닙니다." }
-        }
-
-        fun calculate(operator: String, operand1: Int, operand2: Int): Int {
-            checkArithmeticOperations(operator)
-            return when (operator) {
-                "+" -> add(operand1, operand2)
-                "-" -> subtract(operand1, operand2)
-                "*" -> multiply(operand1, operand2)
-                "/" -> divide(operand1, operand2)
-                else -> -1
+    private fun calculate(splitExpression: List<String>): Double {
+        var result = splitExpression[0].toDouble()
+        for (i in 1 until splitExpression.size step 2) {
+            val operand = splitExpression[i + 1].toDouble()
+            when (splitExpression[i]) {
+                "+" -> result += operand
+                "-" -> result -= operand
+                "*" -> result *= operand
+                "/" -> result /= operand
             }
         }
+        return result
+    }
 
-        fun calculateString(expression: String?): Int {
-            checkNullOrBlank(expression)
-            val splitExpression = expression!!.split(" ")
-            var result = splitExpression[0].toInt()
-            for (i in splitExpression.indices) {
-                if (i % 2 == 0) continue
-                val operator = splitExpression[i]
-                val operand1 = result
-                val operand2 = splitExpression[i + 1].toInt()
-                result = calculate(operator, operand1, operand2)
-            }
-            return result
-        }
+    fun calculateString(expression: String?): Double {
+        val splitExpression = expressionSplit.getSplitExpression(expression)
+        return calculate(splitExpression)
     }
 }
