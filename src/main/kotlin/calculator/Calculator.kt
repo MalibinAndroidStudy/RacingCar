18f60095d06692050bc5ff758c1257a4b386f13d
chaeppy <chaeppy108@gmail.com>
2020-11-02 04:00:14 +0900

    feat(step1): 2차 리팩토링

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,24 +1,20 @@
 package calculator
 
 object Calculator {
-    private val expressionSplit = ExpressionSplit
+    private val splitString = SplitString
 
-    private fun calculate(splitExpression: List<String>): Double {
-        var result = splitExpression[0].toDouble()
-        for (i in 1 until splitExpression.size step 2) {
-            val operand = splitExpression[i + 1].toDouble()
-            when (splitExpression[i]) {
-                "+" -> result += operand
-                "-" -> result -= operand
-                "*" -> result *= operand
-                "/" -> result /= operand
-            }
-        }
-        return result
+    fun calculateExpression(expression: String?): Double {
+        val splitExpression = splitString.getSplitExpression(expression)
+        return calculate(splitExpression)
     }
 
-    fun calculateString(expression: String?): Double {
-        val splitExpression = expressionSplit.getSplitExpression(expression)
-        return calculate(splitExpression)
+    private fun calculate(splitExpression: List<String>): Double {
+        var accumulation = splitExpression[0].toDouble()
+        for (i in 1 until splitExpression.size step 2) {
+            val operator = Operator.findBySymbol(splitExpression[i])
+            val operand = splitExpression[i + 1].toDouble()
+            accumulation = operator.calculate(accumulation, operand)
+        }
+        return accumulation
     }
 }
--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -1,12 +1,14 @@
 import calculator.Calculator
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class CalculatorTest {
-    @Test
-    fun calculateStringTest() {
-        val input = "2 + 3 * 4 / 2"
-        val result = Calculator.calculateString(input)
-        assertThat(result).isEqualTo(((2 + 3) * 4 / 2.0))
+    @DisplayName("식의 계산 결과가 맞는지 테스트")
+    @ParameterizedTest
+    @CsvSource("2 * 3 + 2 / 4,2.0", "3 + 5 / 4 - 1 * 3 ,3.0")
+    fun calculateTest(expression: String, result: Double) {
+        assertThat(Calculator.calculateExpression(expression)).isEqualTo(result)
     }
 }
